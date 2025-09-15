cf7cdfb57c94148f620bfefc54f04a3a3ec5e5a0
Skylar Walsh <skylarwalsh@usf.edu>
2025-09-15 14:30:57 -0400

    document

--- R/myMean.R
+++ R/myMean.R
@@ -3,6 +3,17 @@
 # Here is a corrected version of "myMean" that correctly returns the mean of "assignment2"
 
 # The corrected function
+
+#' myMean()
+#'
+#' @param assignment2 numeric vector
+#'
+#' @returns numeric mean
+#' @export
+#'
+#' @examples
+#' assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+#' myMean(assignment2)
 myMean <- function(assignment2) {
   return(sum(assignment2) / length(assignment2))
 }
